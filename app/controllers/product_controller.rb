a7396caf1fc53b5e299c74c74652a9e3afd00022
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 14:24:45 +0000

    Automatically backed up by Learn

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,10 +1,10 @@
-class ProductController < ApplicationController
+class ProductsController < ApplicationController
   helper_method :cart
   def index
   end
 
   def add
-    cart << params[:product]
-    redirect_to '/product/index'
+    cart << params[:products]
+    redirect_to '/products/index'
   end
 end
